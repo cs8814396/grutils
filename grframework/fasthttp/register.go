8184ae9a692e065a13f2a8e76c4f1a54eaf4ccfd
hezijun <zijunhe@digitalgd.com.cn>
2020-08-28 10:45:11 +0800

    fix

--- grframework/fasthttp/register.go
+++ grframework/fasthttp/register.go
@@ -2,12 +2,12 @@ package fasthttp
 
 import (
 	"encoding/json"
+	//	"errors"
 	"fmt"
 	"github.com/buaazp/fasthttprouter"
 	"github.com/gdgrc/grutils/grapps/config"
 	"github.com/gdgrc/grutils/grframework"
 	"github.com/valyala/fasthttp"
-
 	"reflect"
 	"sync"
 )
@@ -53,11 +53,24 @@ func Register(funcPath string, h interface{}) {
 	})
 	v := reflect.ValueOf(h)
 	t := v.Type()
-	out1ElemName := t.Out(1).Elem().String()
-	if out1ElemName != "grframework.ResponseContext" {
-		msg := fmt.Sprintf("Register func wrong format,out1 element should be ResponseContext, rather than %s", out1ElemName)
+
+	//var tmpErr error
+	//tmpErr = errors.New("")
+
+	errorType := reflect.TypeOf((*error)(nil)).Elem()
+	out1Type := t.Out(0)
+	if !out1Type.Implements(errorType) {
+		//log.Panic("XX(*engin.Context, proto.Message)(proto.Message, error): first out arg must be proto.Message")
+
+		msg := fmt.Sprintf("Register func wrong format,out1 element should be %s rather than %s", errorType.String(), out1Type.String())
 		panic(msg)
 	}
+
+	/*out1ElemName := t.Out(0).Elem().String()
+	if out1ElemName != "error" {
+		msg := fmt.Sprintf("Register func wrong format,out1 element should be error, rather than %s", out1ElemName)
+		panic(msg)
+	}*/
 
 	handler := func(c *fasthttp.RequestCtx) {
 		reqT := t.In(1).Elem()
@@ -94,15 +107,16 @@ func Register(funcPath string, h interface{}) {
 		}
 
 		ret := v.Call([]reflect.Value{reflect.ValueOf(ctx), reqV, rspV})
-		e := ret[0].Interface().(error)
-		if e != nil {
-
-			grError := grframework.MakeError(e)
+		//e := ret[0].Interface().(error)
+		e := ret[0]
+		if !e.IsNil() {
+			tmpErr := e.Interface().(error)
+			grError := grframework.MakeError(tmpErr)
 			defaultResult[grframework.RESULT] = grError.Result
 			defaultResult[grframework.MSG] = grError.Msg
 			return
 		}
-		result = ret[0].Interface()
+		result = rspV.Interface() //ret[0].Interface()
 
 		c.Response.Header.Add("Content-Type", "application/json")
 		//ResponseMap(c, result, false)
